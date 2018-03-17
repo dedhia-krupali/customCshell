bd2d8d171f4d9f01b5e1c33f9d203a6651b1b232
Krupali Dedhia <krupalidedhia30@gmail.com>
2018-03-16 20:55:44 -0500

    Custom C shell

---
+++ version.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "version.h"
+
+// static double version1 = 1.2;
+
+int main() {
+	displayVersion();
+	return 0;
+}
+
---
+++ version.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+// using namespace std;
+
+static char version1[5] = "1.2";
+
+// void displayVersion(){
+// 	printf("%s\n","Version: ", version ); 
+// }
+void displayVersion(){
+	printf("Version: %s\n",version1 );
+	}
